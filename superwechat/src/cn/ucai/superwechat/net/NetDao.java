64f05576333d00b2eb2dfd0928acfd71bec7da39
jwl1 <1305195816@qq.com>
2017-02-09 11:32:29 +0800

    优化登录注册流程，登录成功。

--- superwechat/src/cn/ucai/superwechat/net/NetDao.java
+++ superwechat/src/cn/ucai/superwechat/net/NetDao.java
@@ -2,8 +2,6 @@ package cn.ucai.superwechat.net;
 
 import android.content.Context;
 
-
-
 import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.utils.MD5;
 import cn.ucai.superwechat.utils.OkHttpUtils;
@@ -32,4 +30,27 @@ public class NetDao {
                 .targetClass(String.class)
                 .execute(listener);
     }
+
+    public static void login(Context context, String username, String password,
+                             OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_LOGIN)
+                .addParam(I.User.USER_NAME, username)
+                .addParam(I.User.PASSWORD, MD5.getMessageDigest(password))
+                .targetClass(String.class)
+                .execute(listener);
+
+    }
+
+    public static void getUserInfoByUsername(Context context, String username,
+                                             OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_FIND_USER)
+                .addParam(I.User.USER_NAME, username)
+
+                .targetClass(String.class)
+                .execute(listener);
+
+    }
+
 }
