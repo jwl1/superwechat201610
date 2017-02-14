70702d77981b9b9a182cbe98dacbf3675f148b35
jwl1 <1305195816@qq.com>
2017-02-14 18:38:40 +0800

    查找联系人后显示数据

--- easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
+++ easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
@@ -1,6 +1,7 @@
 package com.hyphenate.easeui.utils;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,6 +14,7 @@ import com.hyphenate.easeui.domain.EaseUser;
 import com.hyphenate.easeui.domain.User;
 
 public class EaseUserUtils {
+    private static final String TAG = EaseUserUtils.class.getSimpleName();
 
     static EaseUserProfileProvider userProvider;
 
@@ -78,12 +80,21 @@ public class EaseUserUtils {
     public static void setAppUserAvatar(Context context, String username, ImageView imageView){
         User user = getAppUserInfo(username);
         if(user != null && user.getAvatar() != null){
+            setAppUserAvatarByPath(context,user.getAvatar(),imageView);
+        }else{
+            Glide.with(context).load(R.drawable.default_hd_avatar).into(imageView);
+        }
+    }
+
+    public static void setAppUserAvatarByPath(Context context,String path,ImageView imageView){
+        Log.e(TAG,"setAppUserAvatarByPath,path="+path);
+        if(path != null){
             try {
-                int avatarResId = Integer.parseInt(user.getAvatar());
+                int avatarResId = Integer.parseInt(path);
                 Glide.with(context).load(avatarResId).into(imageView);
             } catch (Exception e) {
                 //use default avatar
-                Glide.with(context).load(user.getAvatar()).diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(R.drawable.default_hd_avatar).into(imageView);
+                Glide.with(context).load(path).diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(R.drawable.default_hd_avatar).into(imageView);
             }
         }else{
             Glide.with(context).load(R.drawable.default_hd_avatar).into(imageView);
