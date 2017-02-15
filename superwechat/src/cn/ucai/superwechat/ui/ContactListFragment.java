8659c31ada48c0458771826f07b14ebff2e8b420
jwl1 <1305195816@qq.com>
2017-02-15 19:02:05 +0800

    用户详情

---
+++ superwechat/src/cn/ucai/superwechat/ui/ContactListFragment.java
@@ -0,0 +1,327 @@
+/**
+ * Copyright (C) 2016 Hyphenate Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.ucai.superwechat.ui;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Toast;
+
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.easeui.domain.User;
+import com.hyphenate.easeui.ui.EaseContactListFragment;
+import com.hyphenate.superwechat.R;
+import com.hyphenate.util.EMLog;
+import com.hyphenate.util.NetUtils;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+import cn.ucai.superwechat.SuperWechatHelper;
+import cn.ucai.superwechat.SuperWechatHelper.DataSyncListener;
+import cn.ucai.superwechat.db.InviteMessgeDao;
+import cn.ucai.superwechat.db.UserDao;
+import cn.ucai.superwechat.utils.MFGT;
+import cn.ucai.superwechat.widget.ContactItemView;
+
+/**
+ * contact list
+ *
+ */
+public class ContactListFragment extends EaseContactListFragment {
+
+    private static final String TAG = ContactListFragment.class.getSimpleName();
+    private ContactSyncListener contactSyncListener;
+    private BlackListSyncListener blackListSyncListener;
+    private ContactInfoSyncListener contactInfoSyncListener;
+    private View loadingView;
+    private ContactItemView applicationItem;
+    private InviteMessgeDao inviteMessgeDao;
+
+    @SuppressLint("InflateParams")
+    @Override
+    protected void initView() {
+        super.initView();
+        @SuppressLint("InflateParams") View headerView = LayoutInflater.from(getActivity()).inflate(R.layout.em_contacts_header, null);
+        HeaderItemClickListener clickListener = new HeaderItemClickListener();
+        applicationItem = (ContactItemView) headerView.findViewById(R.id.application_item);
+        applicationItem.setOnClickListener(clickListener);
+        headerView.findViewById(R.id.group_item).setOnClickListener(clickListener);
+        listView.addHeaderView(headerView);
+        //add loading view
+        loadingView = LayoutInflater.from(getActivity()).inflate(R.layout.em_layout_loading_data, null);
+        contentContainer.addView(loadingView);
+
+        registerForContextMenu(listView);
+        hideTitleBar();
+    }
+
+    @Override
+    public void refresh() {
+        Map<String,User> m = SuperWechatHelper.getInstance().getAppContactList();
+        if (m instanceof Hashtable<?, ?>) {
+            //noinspection unchecked
+            m = (Map<String, User>) ((Hashtable<String, User>)m).clone();
+        }
+        setContactsMap(m);
+        super.refresh();
+        if(inviteMessgeDao == null){
+            inviteMessgeDao = new InviteMessgeDao(getActivity());
+        }
+        if(inviteMessgeDao.getUnreadMessagesCount() > 0){
+            applicationItem.showUnreadMsgView();
+        }else{
+            applicationItem.hideUnreadMsgView();
+        }
+    }
+
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected void setUpView() {
+        titleBar.setRightImageResource(R.drawable.em_add);
+        titleBar.setRightLayoutClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+//                startActivity(new Intent(getActivity(), AddContactActivity.class));
+                NetUtils.hasDataConnection(getActivity());
+            }
+        });
+        //设置联系人数据
+        Map<String,User> m = SuperWechatHelper.getInstance().getAppContactList();
+        if (m instanceof Hashtable<?, ?>) {
+            m = (Map<String, User>) ((Hashtable<String, User>)m).clone();
+        }
+        setContactsMap(m);
+        super.setUpView();
+        listView.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                User user = (User)listView.getItemAtPosition(position);
+                if (user != null) {
+                 //   String username = user.getMUserName();
+                    // demo中直接进入聊天页面，实际一般是进入用户详情页
+                    MFGT.gotoFirent(getActivity(),user);
+                  //  startActivity(new Intent(getActivity(), ChatActivity.class).putExtra("userId", username));
+                }
+            }
+        });
+
+
+        // 进入添加好友页
+        titleBar.getRightLayout().setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), AddContactActivity.class));
+            }
+        });
+
+
+        contactSyncListener = new ContactSyncListener();
+        SuperWechatHelper.getInstance().addSyncContactListener(contactSyncListener);
+
+        blackListSyncListener = new BlackListSyncListener();
+        SuperWechatHelper.getInstance().addSyncBlackListListener(blackListSyncListener);
+
+        contactInfoSyncListener = new ContactInfoSyncListener();
+        SuperWechatHelper.getInstance().getUserProfileManager().addSyncContactInfoListener(contactInfoSyncListener);
+
+        if (SuperWechatHelper.getInstance().isContactsSyncedWithServer()) {
+            loadingView.setVisibility(View.GONE);
+        } else if (SuperWechatHelper.getInstance().isSyncingContactsWithServer()) {
+            loadingView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (contactSyncListener != null) {
+            SuperWechatHelper.getInstance().removeSyncContactListener(contactSyncListener);
+            contactSyncListener = null;
+        }
+
+        if(blackListSyncListener != null){
+            SuperWechatHelper.getInstance().removeSyncBlackListListener(blackListSyncListener);
+        }
+
+        if(contactInfoSyncListener != null){
+            SuperWechatHelper.getInstance().getUserProfileManager().removeSyncContactInfoListener(contactInfoSyncListener);
+        }
+    }
+
+
+    protected class HeaderItemClickListener implements OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.application_item:
+                    // 进入申请与通知页面
+                    startActivity(new Intent(getActivity(), NewFriendsMsgActivity.class));
+                    break;
+                case R.id.group_item:
+                    // 进入群聊列表页面
+                    startActivity(new Intent(getActivity(), GroupsActivity.class));
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        toBeProcessUser = (User) listView.getItemAtPosition(((AdapterContextMenuInfo) menuInfo).position);
+        toBeProcessUsername = toBeProcessUser.getMUserName();
+        getActivity().getMenuInflater().inflate(R.menu.em_context_contact_list, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.delete_contact) {
+            try {
+                // delete contact
+                deleteContact(toBeProcessUser);
+                // remove invitation message
+                InviteMessgeDao dao = new InviteMessgeDao(getActivity());
+                dao.deleteMessage(toBeProcessUser.getMUserName());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return true;
+        } else if (item.getItemId() == R.id.add_to_blacklist) {
+
+            moveToBlacklist(toBeProcessUsername);
+            return true;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+
+
+    public void deleteContact(final User tobeDeleteUser) {
+        String st1 = getResources().getString(R.string.deleting);
+        final String st2 = getResources().getString(R.string.Delete_failed);
+        final ProgressDialog pd = new ProgressDialog(getActivity());
+        pd.setMessage(st1);
+        pd.setCanceledOnTouchOutside(false);
+        pd.show();
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    EMClient.getInstance().contactManager().deleteContact(tobeDeleteUser.getMUserName());
+                    // remove user from memory and database
+                    UserDao dao = new UserDao(getActivity());
+                    dao.deleteContact(tobeDeleteUser.getMUserName());
+                    SuperWechatHelper.getInstance().getContactList().remove(tobeDeleteUser.getMUserName());
+                    getActivity().runOnUiThread(new Runnable() {
+                        public void run() {
+                            pd.dismiss();
+                            contactList.remove(tobeDeleteUser);
+                            contactListLayout.refresh();
+
+                        }
+                    });
+                } catch (final Exception e) {
+                    getActivity().runOnUiThread(new Runnable() {
+                        public void run() {
+                            pd.dismiss();
+                            Toast.makeText(getActivity(), st2 + e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                }
+
+            }
+        }).start();
+
+    }
+
+    class ContactSyncListener implements DataSyncListener{
+        @Override
+        public void onSyncComplete(final boolean success) {
+            EMLog.d(TAG, "on contact list sync success:" + success);
+            getActivity().runOnUiThread(new Runnable() {
+                public void run() {
+                    getActivity().runOnUiThread(new Runnable(){
+
+                        @Override
+                        public void run() {
+                            if(success){
+                                loadingView.setVisibility(View.GONE);
+                                refresh();
+                            }else{
+                                String s1 = getResources().getString(R.string.get_failed_please_check);
+                                Toast.makeText(getActivity(), s1, Toast.LENGTH_LONG).show();
+                                loadingView.setVisibility(View.GONE);
+                            }
+                        }
+
+                    });
+                }
+            });
+        }
+    }
+
+    class BlackListSyncListener implements DataSyncListener{
+
+        @Override
+        public void onSyncComplete(boolean success) {
+            getActivity().runOnUiThread(new Runnable(){
+
+                @Override
+                public void run() {
+                    refresh();
+                }
+            });
+        }
+
+    }
+
+    class ContactInfoSyncListener implements DataSyncListener{
+
+        @Override
+        public void onSyncComplete(final boolean success) {
+            EMLog.d(TAG, "on contactinfo list sync success:" + success);
+            getActivity().runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    loadingView.setVisibility(View.GONE);
+                    if(success){
+                        refresh();
+                    }
+                }
+            });
+        }
+
+    }
+
+}
