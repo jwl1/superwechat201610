d50cfcb051bfde8c4dafd8c0bdb1b234c95596f5
jwl1 <1305195816@qq.com>
2017-02-17 15:57:29 +0800

    创建一个空的群组

--- superwechat/src/cn/ucai/superwechat/net/NetDao.java
+++ superwechat/src/cn/ucai/superwechat/net/NetDao.java
@@ -2,6 +2,8 @@ package cn.ucai.superwechat.net;
 
 import android.content.Context;
 
+import com.hyphenate.chat.EMGroup;
+
 import java.io.File;
 
 import cn.ucai.superwechat.I;
@@ -102,4 +104,22 @@ public class NetDao {
                 .targetClass(String.class)
                 .execute(listener);
 
-    }}
+    }
+
+    public static void createGroup(Context context, EMGroup group, File file, OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_CREATE_GROUP)
+                .addParam(I.Group.HX_ID,group.getGroupId())
+                .addParam(I.Group.NAME, group.getGroupName())
+                .addParam(I.Group.DESCRIPTION, group.getDescription())
+                .addParam(I.Group.OWNER, group.getOwner())
+                .addParam(I.Group.IS_PUBLIC, String.valueOf(group.isPublic()))
+                .addParam(I.Group.ALLOW_INVITES, String.valueOf(group.isAllowInvites()))
+                .targetClass(String.class)
+              //  .addFile2(file)
+                .post()
+                .execute(listener);
+    }
+
+
+}
